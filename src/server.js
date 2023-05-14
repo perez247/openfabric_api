b0be64d9398f9c1e3787a5c5a8aceb1af53ca109
Excellence Pere <elperez247@gmail.com>
2023-05-14 16:15:45 +0100

    updating cors

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const cors = require('cors')
 const connectDB = require('./config/connectDB');
 
 let app = express();
-// app.use(cors());
+app.use(cors({origin: false}));
 
 // config bady-parser to post data
 app.use(bodyParser.json());
